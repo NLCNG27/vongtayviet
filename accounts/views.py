6485056e9317372983c842870799abaf3a24a97c
Nguyen Lam <nguyen@cngsoftware.com>
2024-05-01 03:48:25 -0500

    init commit

---
+++ accounts/views.py
@@ -0,0 +1,31 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+
+def home(request):
+    return render(request, 'accounts/home.html')
+
+
+# function to sign up
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/register.html', {'form': form})
+
+# function to login
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+    return render(request, 'accounts/login.html')
+
+
